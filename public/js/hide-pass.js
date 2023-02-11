07cb50b4d661528e16a9e0958b52232ad9af04fe
DangMinhDatGCC210147 <dangminhdat14@gmail.com>
2023-02-11 11:44:58 +0700

    first commit

---
+++ public/js/hide-pass.js
@@ -0,0 +1,16 @@
+function myFunction(){
+    var temp = document.getElementById("inputPass");
+    if (temp.type === "password") {
+        temp.type = "text";
+    } else {
+        temp.type = "password";
+    }
+}
+function myFunction1(){
+    var temp = document.getElementById("inputPass1");
+    if (temp.type === "password") {
+        temp.type = "text";
+    } else {
+        temp.type = "password";
+    }
+}
---
+++ src/Entity/index.php
@@ -0,0 +1,44 @@
+<?php
+namespace App\Entity;
+class index{
+        private $username;
+        private $password;
+
+        
+
+        /**
+         * Get the value of username
+         */ 
+        public function getUsername()
+        {
+                return $this->username;
+        }
+
+        /**
+         * Set the value of username
+         */ 
+        public function setUsername($username)
+        {
+                $this->username = $username;
+        }
+
+
+
+
+        /**
+         * Get the value of password
+         */ 
+        public function getPassword()
+        {
+                return $this->password;
+        }
+
+        /**
+         * Set the value of password
+         */ 
+        public function setPassword($password)
+        {
+                $this->password = $password;
+        }
+}
+?>
