5e6c68cee684fa061197d06d1d8cec8e75da0f32
PhucNHGCC210017 <phucnhgcc210017@fpt.edu.vn>
2023-02-17 15:10:02 +0700

    add adding-acount file and AccountController file

---
+++ src/Entity/Account.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AccountRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AccountRepository::class)]
+class Account
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 40)]
+    private ?string $studenName = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $dateOfBirth = null;
+
+    #[ORM\Column(length: 15)]
+    private ?string $major = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $email = null;
+
+    #[ORM\Column(type: Types::SMALLINT)]
+    private ?int $gender = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $password = null;
+
+    #[ORM\Column(type: Types::SMALLINT)]
+    private ?int $role = null;
+
+    #[ORM\Column(length: 9)]
+    private ?string $studentId = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getStudenName(): ?string
+    {
+        return $this->studenName;
+    }
+
+    public function setStudenName(string $studenName): self
+    {
+        $this->studenName = $studenName;
+
+        return $this;
+    }
+
+    public function getDateOfBirth(): ?\DateTimeInterface
+    {
+        return $this->dateOfBirth;
+    }
+
+    public function setDateOfBirth(\DateTimeInterface $dateOfBirth): self
+    {
+        $this->dateOfBirth = $dateOfBirth;
+
+        return $this;
+    }
+
+    public function getMajor(): ?string
+    {
+        return $this->major;
+    }
+
+    public function setMajor(string $major): self
+    {
+        $this->major = $major;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getGender(): ?int
+    {
+        return $this->gender;
+    }
+
+    public function setGender(int $gender): self
+    {
+        $this->gender = $gender;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getRole(): ?int
+    {
+        return $this->role;
+    }
+
+    public function setRole(int $role): self
+    {
+        $this->role = $role;
+
+        return $this;
+    }
+
+    public function getStudentId(): ?string
+    {
+        return $this->studentId;
+    }
+
+    public function setStudentId(string $studentId): self
+    {
+        $this->studentId = $studentId;
+
+        return $this;
+    }
+}
